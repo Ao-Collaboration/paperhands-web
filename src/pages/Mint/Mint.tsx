6222adac8cce1c8329bf43a2c756174153afb7fd
Jono Shields <jonathonshields@gmail.com>
2022-07-20 08:22:32 +1200

    PH-14 only need wallet connection on mint page

--- src/pages/Mint/Mint.tsx
+++ src/pages/Mint/Mint.tsx
@@ -1,9 +1,18 @@
+import { useContext } from 'react'
 import Minting from '../../components/Minting/Minting'
 import PageWrapper from '../../components/PageWrapper/PageWrapper'
+import { Web3Context } from '../../context/Web3/Web3Context'
+import ConnectWallet from '../ConnectWallet/ConnectWallet'
 import useStyles from './Mint.styles'
 
 const Profile: React.FC = () => {
 	const classes = useStyles()
+	const { web3Provider } = useContext(Web3Context)
+
+	if (!web3Provider) {
+		// Require wallet connection
+		return <ConnectWallet />
+	}
 
 	return (
 		<PageWrapper>
