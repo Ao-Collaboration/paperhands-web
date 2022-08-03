eb52e49eee2d0f77d71f2c88ae8dfc4df2a173a0
Michael Standen <michael@aocollab.tech>
2022-08-04 08:49:35 +1200

    PH-15 Update mint page styles

--- src/components/Minting/Minting.styles.tsx
+++ src/components/Minting/Minting.styles.tsx
@@ -1,19 +1,29 @@
 import { createUseStyles } from 'react-jss'
+import { text_shadow } from '../../config/colours'
+import { headingFontFamily } from '../../config/jss-vars'
 
 const styles = {
 	page: {
 		minWidth: '100vw',
+		minHeight: '50vh',
 		display: 'flex',
 		flexDirection: 'column',
 		alignItems: 'center',
 		justifyContent: 'center',
+		'& h1': {
+			fontFamily: headingFontFamily,
+			textTransform: 'uppercase',
+			fontSize: '4em',
+			textShadow: `3px 3px 5px ${text_shadow}`,
+			margin: '0',
+		},
 	},
 	mint: {
 		display: 'flex',
 		flexDirection: 'row',
 		gap: '2em',
 		justifyContent: 'center',
-	}
+	},
 }
 
 export default createUseStyles(styles)
